fd72e9631acea793ae328ba34be6779f5dd9f0e0
Mostafa Hassan <31102305+mhost39@users.noreply.github.com>
2019-12-17 12:47:13 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   API_KEY : "ef01073e1202aa392adfc61542ecd3aa",
   units: "imperial",
   dialogflow: {
-    angularBot: '3d6b5d7aa3874b58b3dbaf7fbcb086dc'
+    angularBot: 'Your_Access_Token'
   }
 };
 
